ff5fac53ce4655e11271f659bc30555830a7ec9a
Turan98 <48137040+Turan98@users.noreply.github.com>
2022-11-05 16:30:46 +0400

    test12

---
+++ for/Program12.cs
@@ -0,0 +1,11 @@
+﻿double a;
+double b = 1;
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("Enter a:" );
+a = Convert.ToDouble(Console.ReadLine() );
+
+for (int i = 1; i  <= a; i++)
+{
+   b = i * i;
+    Console.WriteLine(( i+"^2 = "+b));
+}
